4027935746b391c1beded9702b5c9d75a700f34a
JoyetaBhowmik <joyetabhowmik89@gmail.com>
2025-07-29 21:50:36 +0530

    insret element in end

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main (){
-    printf("nothing is here");
-    return 0;
-}
+  void insertatEnd(int arr[],int n,int ele){
+      arr[n]=ele;
+      for(int i=0;i<=n;i++)
+         printf("%d",arr[i]);
+    
+  }
+  int main(){
+    int n=6;
+    int arr[6]={1,2,3,4,5};
+    int ele=12;
+    insertatEnd(arr,n,ele);
+    return 0;  
+}                             
